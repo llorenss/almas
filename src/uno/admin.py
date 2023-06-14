faf453d792ffb6348480e7720b794b2a0401618f
llorenss <lorens32645@gmail.com>
2023-06-14 18:21:24 +0300

    Add Address model to admin site

--- src/uno/admin.py
+++ src/uno/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Person , Address
 
-# Register your models here.
+@admin.register(Person)
+class PersonAdmin(ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "age",
+    )
+    list_display_links = ("id",)
+    list_editable = (
+        "name",
+        "age",
+    )
+
+@admin.register(Address)
+class AddressAdmin(ModelAdmin):
+    list_display = (
+        "id",
+        "street",
+        "city",
+        "person",
+    )
+    list_display_links = ("id",)
+    list_editable = (
+        "street",
+        "city",
+        "person",
+    )
