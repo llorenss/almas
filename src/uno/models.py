45d597d41649405a9cd3b4d7fd374dd6937e403c
llorenss <lorens32645@gmail.com>
2023-06-14 18:20:13 +0300

    Add Address model in uno app

--- src/uno/models.py
+++ src/uno/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
