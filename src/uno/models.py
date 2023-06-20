76b9a98f70fc9419ee88f9fa649b050f2cedb2ac
llorenss <lorens32645@gmail.com>
2023-06-20 15:32:28 +0300

    Add Model Friendship

--- src/uno/models.py
+++ src/uno/models.py
@@ -9,3 +9,7 @@ class Address(models.Model):
     street = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+class Friendship(models.Model):
+    user= models.ForeignKey(Person, on_delete=models.CASCADE)
+    friend= models.ForeignKey(Person, on_delete=models.CASCADE)
